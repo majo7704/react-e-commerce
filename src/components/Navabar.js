1f81962ad30df4497483ad808d9f26b2e906dcda
j m <jm@js-MacBook-Pro.local>
2019-08-25 15:01:42 +0200

    css

---
+++ src/components/Navabar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navabar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hello form navbar</h3>
+      </div>
+    )
+  }
+}
